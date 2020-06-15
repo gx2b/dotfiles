23dd53e15fb303e5748fb2ca766d155a37d73eb9
matt <git@x2b.org>
2020-06-15 19:20:22 +0200

    added resource files

--- .local/bin/instantscript/ec.sh
+++ .local/bin/instantscript/ec.sh
@@ -4,16 +4,15 @@
 
 # help: ec  -> color picker
 
+xclip -h 2>/dev/null || exit 1
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-LISTFILE="$DIR/colors-name.txt"
+LISTFILE="$DIR/resources/colors-name.txt"
 [[ ! -f "$LISTFILE" ]] && echo "$LISTFILE not found" && exit 1
 
 (($#>1)) && shift $((--OPTIND))
 
-ROFI_MAGIC='-dmenu -markup-rows'
-selected="$(cat "$LISTFILE" | rofi ${ROFI_MAGIC} -p Colors )"
-
-# Exit if nothing is selected
+selected="$(cat "$LISTFILE" | rofi -dmenu -markup-rows -l 10 -width 20 -i -p Colors )"
 [[ -z $selected ]] && exit 1
 
 # get first xml tag
--- .local/bin/instantscript/ee.sh
+++ .local/bin/instantscript/ee.sh
@@ -2,13 +2,15 @@
 
 # help: ee  -> insert emoji
 
-# Must have xclip installed to even show menu.
 xclip -h 2>/dev/null || exit 1
 
-# rofi hat libpango support
-chosen=$(cat ~/.cache/emojis.txt | rofi -dmenu -l 20 -width 30 -i | sed "s/ .*//")
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+LISTFILE="$DIR/resources/emojis.txt"
+[[ ! -f "$LISTFILE" ]] && echo "$LISTFILE not found" && exit 1
 
-[ "$chosen" != "" ] || exit
+# rofi has better emoji support than dmenu because of libpango
+chosen=$(cat $LISTFILE | rofi -dmenu -l 20 -width 30 -i -p Emojis | sed "s/ .*//")
+[[ -z $chosen ]] && exit 1
 
 # If you run this command with an argument, it will automatically insert the character.
 if [ -n "$1" ]; then
